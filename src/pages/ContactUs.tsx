8c9e52a1562cc80b72c86d374f82f8bbc6295464
kamalesh11v <viyanwarkv@rknec.edu>
2024-07-18 13:17:25 +0530

    typeerror fixed

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,18 +1,13 @@
 import  Navbar  from "../components/Navbar"
 import { Footer } from "../components/Footer"
-import { useState } from "react" ;
+
 // import nodemailer , {Transporter} from 'nodemailer';
 
 
 
 
 export const ContactUs = ()=>{
-    const [error, setError] = useState("");
-    const [formInput, setFormInput] = useState({
-        email: "",
-        subject: "",
-        message: ""
-    });
+
 
     // Function to validate email format
     // function validateEmail(email:string) {
@@ -94,9 +89,7 @@ export const ContactUs = ()=>{
                                     id="email" 
                                     className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                                     placeholder="johndoe@gmail.com"
-                                    onChange={(e)=>{
-                                        setFormInput({...formInput , email:e.target.value})
-                                    }}
+                                   
                                       required/>
                                 </div>
                                 <div>
@@ -105,9 +98,7 @@ export const ContactUs = ()=>{
                                     id="subject"
                                     className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                                     placeholder="Let us know how we can help you"
-                                    onChange={(e)=>{
-                                        setFormInput({...formInput , subject : e.target.value})
-                                    }} 
+                                    
                                     required/>
                                 </div>
                                 <div className="sm:col-span-2">
@@ -116,9 +107,7 @@ export const ContactUs = ()=>{
                                     rows={6}
                                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                     placeholder="Leave a comment..."
-                                    onChange={(e)=>{
-                                        setFormInput({...formInput , message : e.target.value})
-                                    }}></textarea>
+                                   ></textarea>
                                 </div>
                                 <button type="submit"
                                 className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
